3463024e9ce26854fcf7659cdccb0413abe2f739
Rachael Metcalf <raespark.games@gmail.com>
2018-03-18 00:35:07 -0500

    comment out code that returns to main menu

--- Scripts/MainLevel.gd
+++ Scripts/MainLevel.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_cancel"):
-		get_tree().change_scene("res://Scenes/MainMenu.tscn")
+		print("CANCEL")
+		#get_tree().change_scene("res://Scenes/MainMenu.tscn")
