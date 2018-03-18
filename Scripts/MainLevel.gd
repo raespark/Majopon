b7e4101b271f928871a9a173d7ab1ee0db31aa91
Rachael Metcalf <raespark.games@gmail.com>
2018-03-18 00:49:30 -0500

    windows and mac initial build

--- Scripts/MainLevel.gd
+++ Scripts/MainLevel.gd
@@ -2,5 +2,4 @@ extends Node2D
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_cancel"):
-		print("CANCEL")
-		#get_tree().change_scene("res://Scenes/MainMenu.tscn")
+		get_tree().change_scene("res://Scenes/MainMenu.tscn")
